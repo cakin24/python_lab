2803ce8a9181599ef12fdd3320979836e79948d1
cakin24 <798103175@qq.com>
2018-12-15 09:12:43 +0800

    cakin

--- socket_clent.py
+++ socket_clent.py
@@ -7,8 +7,19 @@ client =socket.socket()
 ip_port=("127.0.0.1",8888)
 # 连接主机
 client.connect(ip_port)
-# 接收主机信息
-data = client.recv(1024)
-# 打印接收的数据
-# 此处byte型数据特指python3.x以上
-print(data.decode())
+
+#定义一个循环，不断发送消息
+while True:
+    # 接收主机信息
+    data = client.recv(1024)
+    # 打印接收的数据
+    # 此处byte型数据特指python3.x以上
+    print(data.decode())
+    # 输入发送的消息
+    msg_input=input("请输入发送的消息")
+    # 消息发送
+    client.send(msg_input.encode())
+    if msg_input=="exit":
+        break
+    data = client.recv(1024)
+    print(data.decode())
